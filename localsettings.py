e284bff013033a0109145094fcceb913186d31bb
Dipanjan Mukherjee <dipanjan.mu@gmail.com>
2013-03-08 22:43:03 +0530

    Add app to INSTALLED_APPS and Sqlite as a database

--- localsettings.py
+++ localsettings.py
@@ -2,8 +2,8 @@ LOGAN_ROOT = '/home/nirman/code/logan'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': '',                      # Or path to database file if using sqlite3.
+        'ENGINE': 'django.db.backends.sqlite3', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
+        'NAME': LOGAN_ROOT + '/database.db',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': '',
         'PASSWORD': '',
